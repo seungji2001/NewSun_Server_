5df57406855ec6d7e1761251073be6440e447084
JeongHeumChoi <andyc707@gmail.com>
2024-02-10 04:28:17 +0900

    Feat: 프로젝트 init

---
+++ src/main/java/com/newsun/Newsun/NewsunApplication.java
@@ -0,0 +1,13 @@
+package com.newsun.Newsun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsunApplication.class, args);
+	}
+
+}
